0dff8021803a717dbbd929a63d719beb7d0cfb8c
Paul De Ulloa <pauld143@gmail.com>
2023-09-25 19:31:52 -0700

    completed

--- CafeJava.java
+++ CafeJava.java
@@ -5,13 +5,13 @@ public class CafeJava {
         String generalGreeting = "Welcome to Cafe Java, ";
         String pendingMessage = ", your order will be ready shortly";
         String readyMessage = ", your order is ready";
-        String displayTotalMessage = ", Your total is $";
+        String displayTotalMessage = "Your total is $";
         
         // Menu variables (add yours below)
         double mochaPrice = 3.5;
-        double dripCoffee = 4.5;
-        double latte = 4.0;
-        double cappuccino = 5.5;
+        double dripCoffeePrice = 4.5;
+        double lattePrice = 4.0;
+        double cappuccinoPrice = 5.5;
 
     
         // Customer name variables (add yours below)
@@ -29,13 +29,32 @@ public class CafeJava {
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
         
         // Example:
+        //! 1) Cindhuri order
         System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
     	// ** Your customer interaction print statements will go here ** //
-        System.out.println(customer1 + pendingMessage); //for Cindhuri
+        System.out.println(customer1 + pendingMessage);
 
+         //! 2) Noah order
         if(isReadyOrder4) {
-        // what to do if condition is true
-        System.out.println(customer4 + readyMessage + displayTotalMessage + cappuccino);
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + cappuccinoPrice);
         }
+        else{
+            System.out.println(customer4 + pendingMessage);
+        }
+
+        //! 3) Sam Order
+        System.out.println(customer2 + displayTotalMessage + lattePrice * 2);
+        if(isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        }
+        else{
+            System.out.println(customer2 + pendingMessage);
+        }
+
+        //! 4) Jimmy Order
+        System.out.println(customer3);
+        System.out.println(displayTotalMessage + (lattePrice - dripCoffeePrice)+ ", apologies");
+        
     }
 }
